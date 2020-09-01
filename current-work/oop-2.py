8f1a3e6ab49badea270fc3bebdff0cc235d8c5fa
noname45688@gmail.com <csaunders5@liberty.edu>
2020-09-01 16:43:38 -0400

    oop

--- current-work/oop-2.py
+++ current-work/oop-2.py
@@ -14,15 +14,21 @@ class Students(UniversityPeople):
     pass
 
 class Teachers(UniversityPeople):
-    pass
-
+    def __init__(self,fname,lname,email,salary):
+        super().__init__(fname,lname,email) # Inheritence __init__ of UniversityPeople
+        self.salary = salary
 
 stu1 = Students('Christian','Saunders','christiansemail@email.com') # object of Students class
 stu2 = Students('Chris','Saun','chrisemail@email.com') # object of Students class
 
-math = Teachers('Emma','Johnson','email@email.email') # teacher
+math = Teachers('Emma','Johnson','email@email.email', 90000) # teacher
 
 
-print(stu1.fname)
-print(stu2.fname)
-print(math.fname)
+print(stu1.fname) # firstname
+print(stu2.fname) # firstname
+print(math.fname) # firstname
+print(math.salary) # salary
+
+print(isinstance(stu1,Students)) # is stu1 an instance of the class Students
+print(isinstance(stu1, Teachers)) # is stu1 an instance of the class Students
+print(isinstance(stu1,UniversityPeople)) # is stu1 an instance of the class UniversityPeople (it is a subclass)
